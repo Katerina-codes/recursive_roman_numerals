154c390dadb012dd533227fbee8cf2c61d91decd
Katerina <katerinaldg@gmail.com>
2017-06-11 20:46:09 +0100

    It converts 0

--- lib/converter.rb
+++ lib/converter.rb
@@ -1,13 +1,7 @@
 class Converter
 
   def arabic_to_roman(arabic)
-    if arabic == 1
-      "I"
-    elsif arabic == 2
-      "II"
-    else
-      "III"
-    end
+    ""
   end
 
 end
--- spec/converter_spec.rb
+++ spec/converter_spec.rb
@@ -1,20 +1,7 @@
 require 'converter'
 
 describe Converter do
-
-  it "converts arabic 1 to numeral I" do
-    converter = Converter.new
-    expect(converter.arabic_to_roman(1)).to eq("I")
+  it "converts 0" do
+    expect(Converter.new.arabic_to_roman(0)).to eq("")
   end
-
-  it "converts arabic 2 to numeral II" do
-    converter = Converter.new
-    expect(converter.arabic_to_roman(2)).to eq("II")
-  end
-
-  it "converts arabic 3 to numeral III" do
-    converter = Converter.new
-    expect(converter.arabic_to_roman(3)).to eq("III")
-  end
-
 end
