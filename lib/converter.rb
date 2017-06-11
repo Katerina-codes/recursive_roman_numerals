0c4ee44d4f4f0fbf9eb9f81a555a162a5caf4948
Katerina <katerinaldg@gmail.com>
2017-06-11 20:47:39 +0100

    Converts 1

--- lib/converter.rb
+++ lib/converter.rb
@@ -1,7 +1,11 @@
 class Converter
 
   def arabic_to_roman(arabic)
-    ""
+    if arabic == 0
+      ""
+    else
+      "I"
+    end
   end
 
 end
--- spec/converter_spec.rb
+++ spec/converter_spec.rb
@@ -4,4 +4,8 @@ describe Converter do
   it "converts 0" do
     expect(Converter.new.arabic_to_roman(0)).to eq("")
   end
+
+  it "converts 1" do
+    expect(Converter.new.arabic_to_roman(1)).to eq("I")
+  end
 end
